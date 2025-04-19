8985c6423f768cc4b26fea5b05a2a5492d66be7c
k2ttycat <132398023+k2ttycat@users.noreply.github.com>
2025-04-19 23:10:12 +0000

    Improve license information.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # AsekiLinja
+
 A mod loader for Google Snake.
 
 /!\ WARNING! /!\ This code is not working yet!
+
+## License
+
+This project is licensed under the terms of the GNU General Public License v3.0 (GPLv3).
+
+Some source files are under permissive licenses (MIT, BSD-2-Clause). These files retain their original licenses, which are located in the `LICENSES/` directory.
+
+By combining this code, the overall work is licensed under GPLv3. See the LICENSE file for full terms.
--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2025 K2ttycat
+ *
+ * This file is part of AsejiLinja.
+ *
+ * AsejiLinja is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import * as esprima from "esprima";
 import escodegen from "escodegen";
 
@@ -13,7 +32,6 @@ export function init() {
 	window.asekilinja = {
 		applyChanges(code) {
 			const ast = esprima.parseScript(code);
-			console.log(ast.body.length); // just to see it do something.
 			// @ts-ignore escodegen.FORMAT_MINIFY *does* exist.
 			var ret = escodegen.generate(ast, { format: escodegen.FORMAT_MINIFY });
 			return ret;
